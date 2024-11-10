e77f8cb2a7baf419ebdd64992e61465257b0d09d
SergeyKitsul <tiger60510@gmail.com>
2024-11-10 20:18:51 +0200

    10.11.2024 20:18:50,39

---
+++ GitPush.bat
@@ -0,0 +1,14 @@
+git add .
+
+git commit -am "%date% %time%"
+
+git push -f
+
+@chcp 65001
+
+@echo. 
+@echo.
+@echo.
+@echo Изменения опубликованы! Нажмите любую клавишу чтобы выйти ...
+
+@pause > nul
